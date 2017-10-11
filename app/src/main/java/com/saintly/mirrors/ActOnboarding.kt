fc758c0b635e328465d1c466784762fd55f499c1
anmolverma <anmol.verma@mutualmobile.com>
2017-10-11 15:47:06 +0530

    first commit

---
+++ app/src/main/java/com/saintly/mirrors/ActOnboarding.kt
@@ -0,0 +1,12 @@
+package com.saintly.mirrors
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActOnboarding : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act_onboarding)
+    }
+}
